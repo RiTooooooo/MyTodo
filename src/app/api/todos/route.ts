5a13f5d2bcfcd74d5ece1478a7c961ed69615895
RiTooooooo <ryoto0110@icloud.com>
2026-03-22 18:16:49 +0900

    Add Todo model and API spec

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -1,7 +1,33 @@
+import { prismaClient } from 'server/lib/prismaClient';
 import { createRoute } from './frourio.server';
 
-export const { GET } = createRoute({
+export const { GET, POST } = createRoute({
+  // Todo一覧を取得する
   get: async () => {
-    return { status: 200, body: { value: 'ok' } };
+    const todos = await prismaClient.todo.findMany({
+      orderBy: { createdAt: 'desc' },
+    });
+
+    return {
+      status: 200,
+      body: todos.map((todo) => ({
+        ...todo,
+        createdAt: todo.createdAt.toISOString(),
+      })),
+    };
+  },
+  // 新しいTodoを作成する
+  post: async ({ body }) => {
+    const todo = await prismaClient.todo.create({
+      data: { title: body.title },
+    });
+
+    return {
+      status: 201,
+      body: {
+        ...todo,
+        createdAt: todo.createdAt.toISOString(),
+      },
+    };
   },
 });
